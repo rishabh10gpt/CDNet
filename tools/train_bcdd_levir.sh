769467b6635bcff389571a245b07db08f69ab146
rishabh10gpt <31431413+rishabh10gpt@users.noreply.github.com>
2023-06-08 12:15:22 +0530

    Update train_bcdd_levir.sh
    updated training script

--- tools/train_bcdd_levir.sh
+++ tools/train_bcdd_levir.sh
@@ -1,8 +1,3 @@
-#PBS -l select=1:ncpus=128:ngpus=8
-#PBS -q gpu_unlimit
-cd $PBS_O_WORKDIR
-uniq  $PBS_NODEFILE > mpd.hosts
-
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 
 python3 ./tools/train.py --file_root BCDD-LEVIR --lr 5e-5 --max_steps 40000
